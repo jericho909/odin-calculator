ddaaa4d8ec4fd63fff267a62724ad70a5255bd8d
jericho909 <omerahmetyucel@gmail.com>
2023-07-25 14:57:48 +0300

    added ui for playing game

--- javascript.js
+++ javascript.js
@@ -23,68 +23,124 @@ function getComputerChoice(i) {
 }
 
 
-
-
-function getPlayerChoice() {
-    let playerChoice;
-
-    do {
-        playerChoice = prompt("Make your choice: rock, paper or scissors?").toLowerCase()
-    } while (!choices.includes(playerChoice))
-    return playerChoice
-}
-
 let playerScore = 0;
 let cpuScore = 0;
 
 function rockPaperScissors(playerChoice, computerChoice) {
     
     if (playerChoice === computerChoice) {
-        alert(`You chose ${playerChoice}. CPU chose ${computerChoice}. You draw!`)
-        alert(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
+        console.log(`You chose ${playerChoice}. CPU chose ${computerChoice}. You draw!`)
+        console.log(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
     } else if (playerChoice === "rock" && computerChoice === "paper") {
-        alert(`You chose ${playerChoice}. CPU chose ${computerChoice}. You lose!`)
+        console.log(`You chose ${playerChoice}. CPU chose ${computerChoice}. You lose!`)
         cpuScore += 1;
-        alert(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
+        console.log(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
         return cpuScore  
     } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        alert(`You chose ${playerChoice}. CPU chose ${computerChoice}. You win!`)
+        console.log(`You chose ${playerChoice}. CPU chose ${computerChoice}. You win!`)
         playerScore += 1;
-        alert(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
+        console.log(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
         return playerScore
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        alert(`You chose ${playerChoice}. CPU chose ${computerChoice}. You lose!`)
+        console.log(`You chose ${playerChoice}. CPU chose ${computerChoice}. You lose!`)
         cpuScore += 1;
-        alert(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
+        console.log(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
         return cpuScore  
     } else if (playerChoice==="paper" && computerChoice==="rock") {
-        alert(`You chose ${playerChoice}. CPU chose ${computerChoice}. You win!`)
+        console.log(`You chose ${playerChoice}. CPU chose ${computerChoice}. You win!`)
         playerScore += 1;
-        alert(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
+        console.log(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
         return playerScore
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        alert (`You chose ${playerChoice}. CPU chose ${computerChoice}. You lose!`)
+        console.log (`You chose ${playerChoice}. CPU chose ${computerChoice}. You lose!`)
         cpuScore += 1;
-        alert(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
+        console.log(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
         return cpuScore  
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        alert(`You chose ${playerChoice}. CPU chose ${computerChoice}. You win!`)
+        console.log(`You chose ${playerChoice}. CPU chose ${computerChoice}. You win!`)
         playerScore += 1;
-        alert(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
+        console.log(`The current score is: CPU: ${cpuScore}, Player: ${playerScore}`)
         return playerScore
     }
 }
 
-function game() {
-    let counter = 0
-    while (counter < 5) {
-        let playerMove = getPlayerChoice();
-        let computerChoice = Math.floor(Math.random() * 3);
-        let cpuMove = getComputerChoice(computerChoice);
-        rockPaperScissors(playerMove, cpuMove)
-        counter += 1;
-    }
-    alert(`The final score is: CPU: ${cpuScore}, Player: ${playerScore}`)
+function game(playerMove) {
+    
+    let computerChoice = Math.floor(Math.random() * 3);
+    let cpuMove = getComputerChoice(computerChoice);
+    rockPaperScissors(playerMove, cpuMove);
+    
 }
 
-game();
+const btn = document.querySelectorAll('#btn');
+
+
+btn.forEach((button) => {
+    button.addEventListener("click", function(e) {
+        let playerMove = (e.target.innerText);
+        playerMove = playerMove.toLowerCase();
+        game(playerMove);
+        document.getElementById("pScore").textContent = playerScore;
+        document.getElementById("cScore").textContent = cpuScore;
+
+        if (cpuScore === 5 || playerScore === 5) {
+            displayResult();
+        }
+
+    })
+})
+
+let announce;
+let btnParent;
+
+function displayResult() {
+    const newButton = document.createElement("button");
+    newButton.setAttribute("id", "playAgainButton");
+    newButton.textContent = "Play Again?"
+    const cont = document.querySelector(".content");
+    const playButtons = document.querySelectorAll("#btn");
+    console.log(playButtons)
+
+    if (playerScore === 5) {
+        announce = document.createElement("div");
+        console.log("child created")
+        announce.textContent = "YOU WIN! You may play again by using the button below.";
+        announce.setAttribute("id", "resultTable");
+        cont.appendChild(announce);
+        console.log("child appended")
+        btnParent = playButtons[0].parentNode;
+        playButtons.forEach(button => btnParent.removeChild(button));
+    } else if ( cpuScore === 5) {
+        announce = document.createElement("div");
+        console.log("child created")
+        announce.textContent = "YOU LOSE! You may play again by using the button below.";
+        announce.setAttribute("id", "resultTable");
+        cont.appendChild(announce);
+        console.log("child appended")
+        btnParent = playButtons[0].parentNode;
+        playButtons.forEach(button => btnParent.removeChild(button));
+    }
+
+    cont.appendChild(newButton);
+    const startAgain = document.querySelector("#playAgainButton");
+    startAgain.addEventListener ("click", function(e) {
+        cont.removeChild(announce);
+        cont.removeChild(newButton);
+        if (btnParent) {
+            playButtons.forEach(button => btnParent.appendChild(button));
+        }
+
+        playerScore = 0;
+        cpuScore = 0;
+        document.getElementById("pScore").textContent = playerScore;
+        document.getElementById("cScore").textContent = cpuScore;
+    });
+
+
+}
+
+
+
+
+
+
